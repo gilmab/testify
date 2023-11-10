d4f111f44c17df80dd91ef63c8f082899823e9de
gilmab <mabanzag@gmail.com>
2023-11-10 08:40:36 +0100

    modif

--- app/Http/Controllers/VisController.php
+++ app/Http/Controllers/VisController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Product ;
 
 class VisController extends Controller
 {
     public function acceuil(){
+        $product = Product::paginate(3) ; 
+        return view('acceuil.acceuil', compact('product')) ;
        
-        return view('acceuil.acceuil') ;
+       
     }
 }
